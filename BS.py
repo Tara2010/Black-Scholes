9d4ae3b20b5dd100b0674183214c439b81e364c2
Tara2010 <382095845@qq.com>
2017-03-25 15:11:46 -0400

    Update BS.py

--- BS.py
+++ BS.py
@@ -1,6 +1,5 @@
 from scipy.stats import norm
 def call_price(S,K,T,r,vol):
-"""calculate non divident call price"""
     d1 = (log(S/K)+(r+vol**2/2)*T)/(vol*sqrt(T))
     d2 = d1-vol*sqrt(T)
     price = S*norm.cdf(d1)-K*e**(-r*T)norm.cdf(d2)
