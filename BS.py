a88bd82344890d3b34f30ea2d4e7572b2b564a48
Tara2010 <382095845@qq.com>
2017-03-25 15:12:11 -0400

    Update BS.py

--- BS.py
+++ BS.py
@@ -2,6 +2,6 @@ from scipy.stats import norm
 def call_price(S,K,T,r,vol):
     d1 = (log(S/K)+(r+vol**2/2)*T)/(vol*sqrt(T))
     d2 = d1-vol*sqrt(T)
-    price = S*norm.cdf(d1)-K*e**(-r*T)norm.cdf(d2)
+    price = S*norm.cdf(d1)-K*e**(-r*T)*norm.cdf(d2)
     return price
     
